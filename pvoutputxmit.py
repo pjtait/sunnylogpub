659b0523ae91b763c7e51495a11de2d9c290bd2a
Philip Tait <philip@pjtait.org>
2012-03-30 17:35:56 -1000

    Amended to send status every minute.

--- pvoutputxmit.py
+++ pvoutputxmit.py
@@ -29,19 +29,26 @@ class PowerTotal(object):
         self.lastDate = now
         self.lastPower = float(lastdata[29]) * 1000.0
         return self.lastPower
-        
+
+def post_output(date, time, energy, power):
+    dataDict = { 'd' : date, 'pt' : time, 'g' : energy, 'pp' : power}
+    urlApp = 'addoutput.jsp'
+    post_update(dataDict, urlApp)
+    
 def post_status(date, time, energy, power):
+    dataDict = { 'd' : date, 't' : time, 'v1' : energy, 'v2' : power}
+    urlApp = 'addstatus.jsp'
+    post_update(dataDict, urlApp)
+        
+def post_update(dataDict, urlApp):
     '''post status to pvoutput'''
     if opts.url[0:7] != "http://":
         print("URL must be of form http://host/service")
         sys.exit(1)
     url = opts.url[7:].split('/')
     host = url[0]
-    service = '/' + '/'.join(url[1:])
-    params = urllib.urlencode({ 'd' : date,
-                                'pt' : time,
-                                'g' : energy,
-                                'pp' : power})
+    service = '/' + '/'.join(url[1:]) + urlApp
+    params = urllib.urlencode(dataDict)
     headers = {"Content-type": "application/x-www-form-urlencoded",
                "Accept": "text/plain",
                "X-Pvoutput-SystemId" : opts.systemid,
@@ -50,7 +57,7 @@ def post_status(date, time, energy, power):
         print("Connecting to %s" % host)
     conn = httplib.HTTPConnection(host)
     if opts.verbose:
-        print("sending: %s" % params)
+        print("sending: %s to '%s'" % (params, service))
     conn.request("POST", service, params, headers)
     response = conn.getresponse()
     if response.status != 200:
@@ -68,9 +75,10 @@ def proc_data(data):
     g = (float(fields[29])*1000.0)-powerTot.getTotal()
     if opts.dryrun:
         print(date, pt, g, pp)
+        return None
     else:
         if pp > 0.0:
-            post_status(date, pt, g, pp)
+            return(date, pt, g, pp)
     
     
 #proc_data("20120329094917,2.00195e+09,,228,,10,,2,,2.73,,2.73,,2,Mpp-Operation,340,,3.028,,240.6,,59.99,,728,,37.9,,2.242,,483.986,,636.388,,655.995,,463,,2.00195e+09,,822.776,,7,Mpp,3,240V,0,-----,0,-------,")
@@ -79,7 +87,7 @@ parser = OptionParser()
 parser.add_option("", "--apikey", help="pvoutput API key", default=None)
 parser.add_option("", "--systemid", help="pvoutput system ID (an integer)", default=None)
 parser.add_option("", "--url", help="PV output status URL",
-                  default="http://pvoutput.org/service/r2/addoutput.jsp")
+                  default="http://pvoutput.org/service/r2/")
 parser.add_option("", "--verbose", help="be more verbose", action='store_true',
                   default=False)
 parser.add_option("", "--dryrun", help="don't send to pvoutput", action='store_true',
@@ -98,7 +106,8 @@ s = socket. socket(socket. AF_INET, socket. SOCK_DGRAM)
 s. bind(("", port) )
 print "waiting on port: ", port
 powerTot = PowerTotal()
-lastReadTime = datetime.datetime.now() - datetime.timedelta(minutes=5)
+# force first update
+lastStatusTime = datetime.datetime.now() - datetime.timedelta(minutes=10)
 minute = datetime.timedelta(minutes=1)
 while True:
     # Receive up to 1024 bytes in a datagram
@@ -106,7 +115,12 @@ while True:
     if len(data) <= 0:
         continue
     now = datetime.datetime.now()
-    if (now - lastReadTime) > minute:
-        lastReadTime = now
-        proc_data(data)
+    if (now - lastStatusTime) < minute:
+        continue
+    params = proc_data(data)
+    if not params:
+        continue
+    lastStatusTime = now
+    lastStatusTime = now
+    post_status(*params)
     
