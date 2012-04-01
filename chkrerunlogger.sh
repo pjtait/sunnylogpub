c11417ba212aba60fffb2b0fa1de596b3aa00572
Philip Tait <philip@pjtait.org>
2012-04-01 10:44:39 -1000

    Removed hardcode locations.

--- chkrerunlogger.sh
+++ chkrerunlogger.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
+appdir=`dirname $0`
 kill -0 `cat /var/run/sunnylog.pid` 2> /dev/null
 if [ $? != 0 ]; then
 	logger "Restarted sunnylogpub"
-	/home/root/mylogger/sunnylogpubd /home/SunnyData/pvdata
+	${appdir}/sunnylogpubd $1
 fi
